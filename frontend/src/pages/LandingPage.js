a7bd176ee4483c53e62444581005a0dd8f50517a
Seyed Iman Modarres Sadeghi <imanmodarres1381@gmail.com>
2023-12-27 10:04:02 -0500

    changed some names

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -9,85 +9,85 @@ import "./LandingPage.css";
 import mockupImg from "../static/images/mockup_blue.png";
 
 const useStyles = makeStyles((theme) => ({
-	root: {
-		flexGrow: 1,
-		background:
-			"linear-gradient(90deg, rgba(164, 174, 223, 1) 9%, rgba(255, 112, 160, 1) 100%)",
-	},
+  root: {
+    flexGrow: 1,
+    background:
+      "linear-gradient(90deg, rgba(164, 174, 223, 1) 9%, rgba(255, 112, 160, 1) 100%)",
+  },
 }));
 
 function LandingPage() {
-	const classes = useStyles();
-	return (
-		<Grid container className={classes.root}>
-			<Grid
-				className="LandingPage__mainText"
-				container
-				item
-				xs={12}
-				md={7}
-				direction="column"
-				justify="center"
-				alignItems="flex-start"
-			>
-				<Box fontWeight={300} pl={5} textAlign="left" py={3} mr={2}>
-					<Typography
-						variant="h3"
-						style={{ fontWeight: "200", color: "3f51b5" }}
-					>
-						Create your digital business card with OneLink
-					</Typography>
-				</Box>
-				<Box pl={5} pb={4} mr={2} textAlign="left">
-					<Typography variant="h6">
-						All the ways your clients can contact you, in one link
-					</Typography>
-				</Box>
-				<Box display="flex" flexDirection="row" pb={3} alignSelf="center">
-					<Button
-						variant="contained"
-						color="secondary"
-						size="large"
-						style={{
-							margin: "0 .5rem",
-						}}
-						href="https://www.linkedin.com/feed/update/urn:li:activity:6761314380282920961/"
-					>
-						Watch Demo
-					</Button>
-					<Button
-						variant="contained"
-						color="primary"
-						size="large"
-						style={{ margin: "0 .5rem" }}
-						component={routerLink}
-						to="/signup"
-					>
-						Start Now
-					</Button>
-				</Box>
-			</Grid>
-			<Grid
-				className="LandingPage__imageContainer"
-				container
-				item
-				xs={12}
-				md={5}
-				direction="column"
-				justify="center"
-				alignItems="center"
-			>
-				<Box
-					display="flex"
-					style={{ height: "100%" }}
-					justifyContent="center"
-					alignItems="center"
-				>
-					<img src={mockupImg} alt="mockup" style={{ height: "92%" }} />
-				</Box>
-			</Grid>
-		</Grid>
-	);
+  const classes = useStyles();
+  return (
+    <Grid container className={classes.root}>
+      <Grid
+        className="LandingPage__mainText"
+        container
+        item
+        xs={12}
+        md={7}
+        direction="column"
+        justify="center"
+        alignItems="flex-start"
+      >
+        <Box fontWeight={300} pl={5} textAlign="left" py={3} mr={2}>
+          <Typography
+            variant="h3"
+            style={{ fontWeight: "200", color: "3f51b5" }}
+          >
+            Create your digital business card with InfoCard
+          </Typography>
+        </Box>
+        <Box pl={5} pb={4} mr={2} textAlign="left">
+          <Typography variant="h6">
+            All the ways your clients can contact you, in InfoCard
+          </Typography>
+        </Box>
+        <Box display="flex" flexDirection="row" pb={3} alignSelf="center">
+          <Button
+            variant="contained"
+            color="secondary"
+            size="large"
+            style={{
+              margin: "0 .5rem",
+            }}
+            href="https://www.linkedin.com/feed/update/urn:li:activity:6761314380282920961/"
+          >
+            Watch Demo
+          </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            style={{ margin: "0 .5rem" }}
+            component={routerLink}
+            to="/signup"
+          >
+            Start Now
+          </Button>
+        </Box>
+      </Grid>
+      <Grid
+        className="LandingPage__imageContainer"
+        container
+        item
+        xs={12}
+        md={5}
+        direction="column"
+        justify="center"
+        alignItems="center"
+      >
+        <Box
+          display="flex"
+          style={{ height: "100%" }}
+          justifyContent="center"
+          alignItems="center"
+        >
+          <img src={mockupImg} alt="mockup" style={{ height: "92%" }} />
+        </Box>
+      </Grid>
+    </Grid>
+  );
 }
 
 export default LandingPage;
--- frontend/src/pages/SignIn.js
+++ frontend/src/pages/SignIn.js
@@ -20,163 +20,163 @@ import { Link as RouterLink, useHistory } from "react-router-dom";
 import "./SignIn.css";
 
 function Copyright() {
-	return (
-		<Typography variant="body2" color="textSecondary" align="center">
-			{"Copyright © "}
-			<Link color="inherit" href="https://material-ui.com/">
-				OneLink
-			</Link>{" "}
-			{new Date().getFullYear()}
-			{"."}
-		</Typography>
-	);
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://material-ui.com/">
+        InfoCard
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
 }
 
 const useStyles = makeStyles((theme) => ({
-	paper: {
-		// marginTop: theme.spacing(8),
-		display: "flex",
-		flexDirection: "column",
-		alignItems: "center",
-	},
-	avatar: {
-		margin: theme.spacing(1),
-		backgroundColor: theme.palette.secondary.main,
-	},
-	sigInForm: {
-		padding: theme.spacing(1),
-	},
-	form: {
-		width: "100%", // Fix IE 11 issue.
-		marginTop: theme.spacing(1),
-	},
-	submit: {
-		margin: theme.spacing(3, 0, 2),
-	},
+  paper: {
+    // marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  sigInForm: {
+    padding: theme.spacing(1),
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
 }));
 
 export default function SignIn() {
-	const history = useHistory();
-	const classes = useStyles();
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
+  const history = useHistory();
+  const classes = useStyles();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-	const dispatch = useDispatch();
-	const userLogin = useSelector((state) => state.userLogin);
-	const { loading, loginError, userInfo } = userLogin;
+  const dispatch = useDispatch();
+  const userLogin = useSelector((state) => state.userLogin);
+  const { loading, loginError, userInfo } = userLogin;
 
-	useEffect(() => {
-		if (userInfo && userInfo.token) {
-			history.push("/dashboard");
-		}
-	}, [userInfo, history]);
+  useEffect(() => {
+    if (userInfo && userInfo.token) {
+      history.push("/dashboard");
+    }
+  }, [userInfo, history]);
 
-	const submitHandler = (event) => {
-		event.preventDefault();
-		dispatch(loginUser(email, password));
-	};
+  const submitHandler = (event) => {
+    event.preventDefault();
+    dispatch(loginUser(email, password));
+  };
 
-	return (
-		<div className="SignIn__container">
-			<Container component="main" maxWidth="xs" className="signInForm">
-				<div className="blur"></div>
-				<CssBaseline />
-				<div className={classes.paper}>
-					<Avatar className={classes.avatar}>
-						<LockOutlinedIcon />
-					</Avatar>
-					<Typography component="h1" variant="h5">
-						Sign in
-					</Typography>
-					<form className={classes.form} onSubmit={submitHandler}>
-						<TextField
-							onChange={(e) => {
-								setEmail(e.target.value);
-							}}
-							variant="outlined"
-							margin="normal"
-							required
-							fullWidth
-							id="email"
-							label="Email Address"
-							name="email"
-							autoComplete="email"
-							autoFocus
-						/>
-						<TextField
-							onChange={(e) => {
-								setPassword(e.target.value);
-							}}
-							variant="outlined"
-							margin="normal"
-							required
-							fullWidth
-							name="password"
-							label="Password"
-							type="password"
-							id="password"
-							autoComplete="current-password"
-						/>
-						<FormControlLabel
-							control={<Checkbox value="remember" color="primary" />}
-							label="Remember me"
-						/>
-						<Button
-							type="submit"
-							fullWidth
-							variant="contained"
-							color="primary"
-							className={classes.submit}
-						>
-							Sign In
-						</Button>
-						{loginError && (
-							<Grid
-								container
-								direction="column"
-								justify="center"
-								alignItems="center"
-								style={{ marginBottom: ".5rem" }}
-							>
-								<Alert
-									severity="error"
-									variant="outlined"
-									style={{ backgroundColor: "#ff000012" }}
-								>
-									<AlertTitle>Error</AlertTitle>
-									{loginError}
-								</Alert>
-							</Grid>
-						)}
-						{loading && (
-							<Grid
-								container
-								direction="column"
-								justify="center"
-								alignItems="center"
-							>
-								<CircularProgress></CircularProgress>
-							</Grid>
-						)}
+  return (
+    <div className="SignIn__container">
+      <Container component="main" maxWidth="xs" className="signInForm">
+        <div className="blur"></div>
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <form className={classes.form} onSubmit={submitHandler}>
+            <TextField
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign In
+            </Button>
+            {loginError && (
+              <Grid
+                container
+                direction="column"
+                justify="center"
+                alignItems="center"
+                style={{ marginBottom: ".5rem" }}
+              >
+                <Alert
+                  severity="error"
+                  variant="outlined"
+                  style={{ backgroundColor: "#ff000012" }}
+                >
+                  <AlertTitle>Error</AlertTitle>
+                  {loginError}
+                </Alert>
+              </Grid>
+            )}
+            {loading && (
+              <Grid
+                container
+                direction="column"
+                justify="center"
+                alignItems="center"
+              >
+                <CircularProgress></CircularProgress>
+              </Grid>
+            )}
 
-						<Grid container justify="center">
-							<Grid item>
-								<Button
-									variant="text"
-									color="primary"
-									component={RouterLink}
-									to="/signup"
-								>
-									Don't have an account? Sign Up
-								</Button>
-							</Grid>
-						</Grid>
-					</form>
-				</div>
-				<Box mt={8}>
-					<Copyright />
-				</Box>
-			</Container>
-		</div>
-	);
+            <Grid container justify="center">
+              <Grid item>
+                <Button
+                  variant="text"
+                  color="primary"
+                  component={RouterLink}
+                  to="/signup"
+                >
+                  Don't have an account? Sign Up
+                </Button>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+        <Box mt={8}>
+          <Copyright />
+        </Box>
+      </Container>
+    </div>
+  );
 }
